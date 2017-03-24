02ddd03e2526ea807592d3b7165e310c83c06b0d
Unknown <grahulte@gmail.com>
2017-03-24 19:38:38 +0000

    Selects only good emails
    Works perfectly with chrome console, but with the extension only the addresses of the first div are displayed.

--- content.js
+++ content.js
@@ -1,13 +1,25 @@
-window.addEventListener ("load", myMain, false);
+window.addEventListener("load", myMain, false);
 
-function myMain (evt) {
-    function extractEmails (text)
-    {
+function myMain(evt) {
+
+    $('.icon-sm.icon-overflow-menu-horizontal').click(function(){
+        setTimeout(function () {
+            $('.pop-over-list').append('<li><a>Copy emails to clipboard</a></li>');
+        },50);
+    });
+
+
+    function extractEmails(text) {
         return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
     }
+
     $('.js-list.list-wrapper').each(function (i) {
-        let email = $(this).find('.list-card-title.js-card-name').text().trim();
-        email = extractEmails(email);
-        console.log(email);
+        $(this).find('.list-card.js-member-droppable.ui-droppable').each(function (j) {
+            //if (!$(this).find('.card-label').hasClass('card-label-black')) {
+                let email = $(this).find('.list-card-title.js-card-name').text().trim();
+                email = extractEmails(email);
+                console.log('bloc ' + i + ' ligne ' + j + ' ' + email);
+            //}
+        });
     });
 }
